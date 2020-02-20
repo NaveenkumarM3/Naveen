a3b8be049f0328969889937ba08d9d3d8d5e84d7
N.rithinP-hexaware <N.rithinP@hexaware.com>
2020-02-20 11:47:59 +0530

    rrr

---
+++ LEAVEMANAGEMENT.sql
@@ -0,0 +1,10 @@
+
+show databases;
+use mlp213;
+show tables;
+select * from leaverequest;
+select * from employee;
+  update employee set ename='Ram (CEO)' where ename='ram';
+  set sql_safe_updates=0;
+  create table rithin001(id int,name varchar(20));
+  
